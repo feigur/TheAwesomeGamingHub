ba2bd4badac5aa18f8d266bed71e8507a538ee39
feigur <ofeigur95@gmail.com>
2022-03-08 16:31:38 +0000

    s

--- app/src/main/java/com/example/awesomegaminghub/MainActivity.java
+++ app/src/main/java/com/example/awesomegaminghub/MainActivity.java
@@ -15,6 +15,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.awesomegaminghub.databinding.ActivityMainBinding;
+import com.example.awesomegaminghub.databinding.FragmentLoginBinding;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home, R.id.nav_chat, R.id.nav_news)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
