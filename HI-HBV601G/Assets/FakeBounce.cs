0b325c81e7028e4bcce66029e724aede66cf6c8f
HinnDraugurinn <mao54@hi.is>
2022-03-15 18:03:14 +0000

    Integrated Unity project into native Android project

---
+++ HI-HBV601G/Assets/FakeBounce.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class FakeBounce : MonoBehaviour
+{
+    void Update()
+    {
+        transform.position = new Vector3(0, 0.7f * Mathf.Sin(2f * Time.time) + 1.2f, 0);
+    }
+}
