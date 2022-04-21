601b215b75b0db6ab666e590f11f33d360b005f0
HinnDraugurinn <mao54@hi.is>
2022-04-21 21:15:02 +0000

    Minor Modifications

--- app/src/main/java/com/example/awesomegaminghub/ui/home/GamePageFragment.java
+++ app/src/main/java/com/example/awesomegaminghub/ui/home/GamePageFragment.java
@@ -1,7 +1,15 @@
 package com.example.awesomegaminghub.ui.home;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.Fragment;
 
 import android.os.Handler;
@@ -15,11 +23,13 @@ import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import com.example.awesomegaminghub.MainActivity;
+import com.example.awesomegaminghub.MainUnityActivity;
 import com.example.awesomegaminghub.R;
 import com.example.awesomegaminghub.databinding.FragmentGamePageBinding;
 import com.example.awesomegaminghub.databinding.FragmentLoginBinding;
 import com.example.awesomegaminghub.entities.Account;
 import com.example.awesomegaminghub.entities.HighScore;
+import com.google.gson.Gson;
 
 import org.w3c.dom.Text;
 
@@ -29,6 +39,18 @@ import java.util.List;
 import java.util.Map;
 
 public class GamePageFragment extends Fragment {
+
+    boolean isUnityLoaded = false;
+
+    ActivityResultLauncher<Intent> gameActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                    }
+                }
+            });
 
     private FragmentGamePageBinding binding;
     private ArrayAdapter<String> highscoreAdapter;
@@ -40,6 +62,9 @@ public class GamePageFragment extends Fragment {
 
     private HighScore highScore;
     private int count;
+
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor spEditor;
 
     private Handler handler = new Handler();
     private Runnable runnable;
@@ -60,20 +85,33 @@ public class GamePageFragment extends Fragment {
         chosenGame.setText("You are playing: " + gameName);
         View root = binding.getRoot();
         getData();
+
+        gameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnLoadUnity(v);
+            }
+        });
         return root;
     }
 
+    @Override
+    public void onResume () {
+        super.onResume();
+        getData();
+    }
+
     private HighScore getHighScore(){
-        if(gameName.equals("ordle")){
+        if(gameName.equals("Orðle")){
             gameId = "1";
         }
-        else if(gameName.equals("minesweeper")){
+        else if(gameName.equals("Minesweeper")){
             gameId = "2";
         }
-        else if(gameName.equals("lavahazard")){
+        else if(gameName.equals("Lava Hazard")){
             gameId = "3";
         }
-        else if(gameName.equals("")){
+        else if(gameName.equals("Snákur")){
             gameId = "4";
         }
         return ((MainActivity)getActivity()).getHighScore(gameId);
@@ -110,6 +148,22 @@ public class GamePageFragment extends Fragment {
             buff = usernamesHighscore.get(i) + ": " + scoresHighscore.get(i).toString();
             highscoreAdapter.add(buff);
         }
+    }
+
+    public void btnLoadUnity(View v) {
+        isUnityLoaded = true;
+        Intent intent = new Intent(getActivity(), MainUnityActivity.class);
+        intent.putExtra("Game Name", gameName);
+        sharedPreferences = getActivity().getPreferences(Context.MODE_PRIVATE);
+        spEditor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("loggedUser", "");
+        Account user = gson.fromJson(json, Account.class);
+        intent.putExtra("Username", user.getUsername());
+        //Intent intent = new Intent(this, MainUnityActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        gameActivityResultLauncher.launch(intent);
+        //startActivityForResult(intent, 1);
     }
 
     @Override
