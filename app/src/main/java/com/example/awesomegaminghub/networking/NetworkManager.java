55d3bb9d783b93ad660cefd4720db2250d76879b
feigur <ofeigur95@gmail.com>
2022-04-22 12:20:16 +0000

    AppwithGame

--- app/src/main/java/com/example/awesomegaminghub/networking/NetworkManager.java
+++ app/src/main/java/com/example/awesomegaminghub/networking/NetworkManager.java
@@ -22,7 +22,8 @@ import java.util.FormatFlagsConversionMismatchException;
 import java.util.List;
 
 public class NetworkManager {
-    private static final String BASE_URL = "http://192.168.8.100:8080/";
+    private static final String VIRTUAL_URL = "http://10.0.2.2:8080/";
+    private static final String BASE_URL = "http://192.168.1.5:8080/";
 
     private static NetworkManager mInstance;
     private static RequestQueue mQueue;
